9bd51d302ec0759cb8cfb313035ef50d6078b525
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-01-18 08:55:19 +0900

    bug

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const insert = document.getElementById('insert')
 window.addEventListener('keydown', (event) => {
   insert.innerHTML = `
     <div class="key">
-      ${event.key === '' ? 'Space' : event.key}
+      ${event.key === ' ' ? 'Space' : event.key}
       <small>event.key</small>
     </div>
     <div class="key">
