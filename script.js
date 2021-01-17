8532373fcfea5a8c5cee1b97a0f331474d3be977
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-01-18 08:06:12 +0900

    Neumorphism CSS Effect　keyCode Fetcher

--- script.js
+++ script.js
@@ -1,39 +1,17 @@
-const jokeEle = document.getElementById('joke')
-const jokeBtn = document.getElementById('jokeBtn')
-const titleEle = document.getElementById('title')
+const insert = document.getElementById('insert')
 
-const message1 = 'Can you laugh?'
-const message2 = 'Go through these jokes until you laugh'
-
-jokeBtn.addEventListener('click', generateJoke)
-
-generateJoke()
-
-function toggleTitle() {
-  titleEle.innerHTML = titleEle.textContent === message1 ? message2 : message1
-}
-
-async function generateJoke() {
-    const config = {
-      headers: {
-        Accept: 'application/json',
-      },
-    }
-    const res = await fetch('https://icanhazdadjoke.com', config)
-    const data = await res.json()
-  jokeEle.innerHTML = data.joke
-
-  toggleTitle()
-
-// function generateJoke() {
-//   const config = {
-//     headers: {
-//       Accept: 'application/json',
-//     },
-//   }
-//   fetch('https://icanhazdadjoke.com', config)
-//     .then(res => res.json())
-//     .then(data => {
-//       jokeEle.innerHTML=data.joke
-//     })
-}
+window.addEventListener('keydown', (event) => {
+  insert.innerHTML = `
+    <div class="key">
+      ${event.key === '' ? 'Space' : event.key}
+      <small>event.keyCode</small>
+    </div>
+    <div class="key">
+      ${event.keyCode}
+      <small>event.keyCode</small>
+    </div>
+    <div class="key">
+      ${event.code}
+      <small>event.code</small>
+    </div>`
+})
